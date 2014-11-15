a38cc82ddac2c4336eabe8cc925d20b932934c8f
Anton Lindgren <lindgren.anton@gmail.com>
2014-11-15 09:58:11 +0100

    Reject non-registered teams

--- server/game-router.js
+++ server/game-router.js
@@ -6,6 +6,8 @@ var bodyParser = require('body-parser');
 var validate = require('./validate.js');
 var game = require('./game.js');
 var sockets = require('./sockets');
+var db = require('./db');
+var log = require('./log');
 
 game.on('gameover', function (results) {
   sockets.sendReplayTo(results.gameId, results.replay);
@@ -39,23 +41,37 @@ router.post('/', function (req, res) {
   // Like to a client that is watching the replay (which does not require authentication)
   delete req.body.apiKey;
 
-  // Join game is a special server-only command that
-  // the game engine knows nothing about, so let's handle
-  // it separately
-  if (req.body.command === 'join game') {
-    game.joinGame(req.body.gameId, req.body.team, function (err, stateForPlayer) {
-      if (err) return res.status(BAD_REQUEST).send({ message: err });
+  db.isTeamRegistered(req.body.team, function (err, isRegistered) {
+    if (err) {
+      log.error(err);
+      res.status(500).render('error');
+      return;
+    }
 
-      res.send(stateForPlayer);
-    });
-  } else {
-    // Execute the command requested
-    game.executeTurn(req.body, function (err, stateForPlayer) {
-      if (err) return res.status(BAD_REQUEST).send({ message: err });
+    if (!isRegistered) {
+      res.status(400).send({ message: 'Invalid team name' });
+      return;
+    }
 
-      res.send(stateForPlayer);
-    });
-  }
+
+    // Join game is a special server-only command that
+    // the game engine knows nothing about, so let's handle
+    // it separately
+    if (req.body.command === 'join game') {
+      game.joinGame(req.body.gameId, req.body.team, function (err, stateForPlayer) {
+        if (err) return res.status(BAD_REQUEST).send({ message: err });
+
+        res.send(stateForPlayer);
+      });
+    } else {
+      // Execute the command requested
+      game.executeTurn(req.body, function (err, stateForPlayer) {
+        if (err) return res.status(BAD_REQUEST).send({ message: err });
+
+        res.send(stateForPlayer);
+      });
+    }
+  });
 });
 
 module.exports = router;
