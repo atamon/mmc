b6fe309589ca49bcb29565ebe0bda6a4bd38a9e3
Anton Lindgren <lindgren.anton@gmail.com>
2014-11-17 08:52:40 +0100

    Implement real level look-up

--- server/levels.js
+++ server/levels.js
@@ -1,10 +1,16 @@
 var levelMap = {};
-// var path = require('path');
-// var fs = require('fs');
+var path = require('path');
+var fs = require('fs');
 
-// TODO Load all from ../levels instead
-levelMap.test = require('../levels/test.json');
-levelMap.battle = require('../levels/battle.json');
+var files = fs.readdirSync(path.resolve(__dirname + '/../levels/'));
+
+files.forEach(function (fileName) {
+  if (/(\.json)$/gi.test(fileName)) {
+    var filePath = path.resolve(__dirname + '/../levels/' + fileName);
+    var string = fs.readFileSync(filePath, 'utf-8');
+    levelMap[fileName.replace('.json', '')] = JSON.parse(string);
+  }
+});
 
 var get = function (levelId) {
   return levelMap[levelId];
