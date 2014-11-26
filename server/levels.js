4927f985420be5ed961c08de76594cc8c380ad83
Anton Lindgren <lindgren.anton@gmail.com>
2014-11-26 20:58:12 +0100

    Require levels from monkey-music

--- server/levels.js
+++ server/levels.js
@@ -1,19 +1,12 @@
-var levelMap = {};
-var path = require('path');
-var fs = require('fs');
 
-var files = fs.readdirSync(path.resolve(__dirname + '/../levels/'));
-
-files.forEach(function (fileName) {
-  if (/(\.json)$/gi.test(fileName)) {
-    var filePath = path.resolve(__dirname + '/../levels/' + fileName);
-    var string = fs.readFileSync(filePath, 'utf-8');
-    levelMap[fileName.replace('.json', '')] = JSON.parse(string);
-  }
-});
-
+var log = require('./log');
 var get = function (levelId) {
-  return levelMap[levelId];
+  try {
+    return require('monkey-music/levels/' + levelId + '.json');
+  } catch (e) {
+    log.error('Failed to find level ' + levelId);
+    return false;
+  }
 };
 
 exports.get = get;
